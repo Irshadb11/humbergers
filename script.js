5a28f8aa8ed2faf6d61ea9c69790c485b1e7c4b9
Irshadb11 <ib9826560@gmail.com>
2024-08-01 10:55:47 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+
+let menuList = document.getElementById("menuList")
+
+// menuList.style.maxHeight = "0px"
+
+function toogleMenu() {
+    if (menuList.style.maxHeight == "0px") {
+        menuList.style.maxHeight = "300px"
+    }
+    else{
+        menuList.style.maxHeight = "0px"
+
+    }
+}
